f8ac4366eebf8394433593120a98a0ca0ab83efc
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-16 13:14:14 +0545

    created basic serializer for auth models

---
+++ authentication/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import User, Administrator, Teacher, Student
+
+# class UserSerializer(serializers.ModelSerializer):
+    
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'user_type')
+
+
+class AdministratorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Administrator
+        fields = ('id', 'user')
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teacher
+        fields = ('id', 'user')
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = ('id', 'user')
