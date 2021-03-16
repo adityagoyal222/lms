7fac16c56a76e988bd4cd4baa572160c98995770
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-16 11:51:48 +0545

    registered models in admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Administrator, Student, Teacher
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Administrator)
+admin.site.register(Student)
+admin.site.register(Teacher)
