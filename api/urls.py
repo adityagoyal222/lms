c905bc61c1532044304233a69bd882dc924454eb
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-21 20:00:25 +0545

    created urls for student and teacher authentication

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from authentication.views import register_admin, delete_admin, get_admin, login_user, ChangePasswordView
+from authentication.views import (register_admin, delete_admin,
+                                get_admin, login_user,
+                                ChangePasswordView, register_student,
+                                delete_student, get_student,
+                                register_teacher, delete_teacher,
+                                get_teacher)
 
 app_name="api"
 
@@ -14,6 +19,12 @@ urlpatterns = [
     url(r'^admin/$', get_admin),
     url(r'^admin/create/', register_admin),
     url(r'^admin/delete/', delete_admin),
+    url(r'^student/$', get_student),
+    url(r'^student/create/', register_student),
+    url(r'^student/delete/', delete_student),
+    url(r'^teacher/$', get_teacher),
+    url(r'^teacher/create/', register_teacher),
+    url(r'^teacher/delete/', delete_teacher),
     url(r'^login/', login_user),
     url(r'^token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
